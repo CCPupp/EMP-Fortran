332b1b96ff6f56de76baad363011abca7ab257c8
clawwood <woodward.41@wright.edu>
2019-04-21 21:34:14 -0400

    Add files via upload

--- SOURCE.py
+++ SOURCE.py
@@ -1,4 +1,19 @@
 '''
+13 COMMON variables in SOURCEN: IFUN, CAY, ALPHA, BETA, TPEAK, PEAK2, DT2, TC1, ALPHA1, BETA1, TC2, SOF, NSOF
+Variable 'T' is a parameter passed to the function
+Note that variables in all-caps are COMMON, while the rest are local
+TABLIN (in line 59) is a function defined elsewhere in the program
+
+Possible problems:
+--This is a literal translation of the code, which has problems as pointed out in the meeting on 4/11.
+Specifically, local variables 'a' and 'b' are calculated in lines 51 and 52, respectively, but are never used.
+They would be used in lines 55 or 57, but as written, the code would generate an error if it reached those lines
+	since the variables would be undefined.
+The contact suggested that values that would transfer control to the problematic lines
+	(specifically, in which (IFUN == 3) AND (T + DT2 – TC1 > 0))
+	were never run through the system to generate an error.
+
+Original FORTRAN code (block of COMMON variables omitted):
 GO  TO  (10, 20, 30, 100),  IFUN
 10	SOURCE=CAY * EXP(ALPHA * T) / (1.0 + EXP( ( ALPHA + BETA) * (T – TPEAK) ) )
 	RETURN
@@ -25,16 +40,22 @@ import math
 
 def SOURCE(T, IFUN, CAY, ALPHA, BETA, TPEAK, PEAK2, DT2, TC1, ALPHA1, BETA1, TC2, SOF, NSOF) :
 	result = 0
-	if (IFUN == 10) :
+	if (IFUN <= 1 or IFUN > 4) : #10
 		result = CAY * math.exp(ALPHA * T) / (1.0 + math.exp((ALPHA + BETA) * (T – TPEAK)))
-	elif (IFUN == 20) :
+	elif (IFUN == 2) : #20
 		SO = math.exp(ALPHA * T) / (1.0 + math.exp((ALPHA + BETA) * (T – TPEAK)))
-		result=CAY * SO * (2.0 – SO / PEAK2)
-	elif (IFUN == 30) :
-		if (T + DT2 – TC1 <= 0) : #40, 40 , 70
-			if  (T – DT2 – DT <= 0) : #50, 50 , 60
-				A = math.exp((ALPHA1 – ALPHA) * TC1) * (1.0 + math.exp((ALPHA + BETA) * (TC1 – TPEAK)))
-				B = A * math.exp((ALPHA + BETA1) * TC2) / (1.0 + math.exp((ALPHA + BETA) * (TC2 – TPEAK)))
-				result = CAY * math.exp(ALPHA1 * T)
-			else :
-				
+		result = CAY * SO * (2.0 – SO / PEAK2)
+	elif (IFUN == 3) : #30
+		if (T + DT2 – TC1 <= 0) : #directs to 40, 40 , 70
+			if  (T – DT2 – DT <= 0) : #40; directs to 50, 50 , 60
+				a = math.exp((ALPHA1 – ALPHA) * TC1) * (1.0 + math.exp((ALPHA + BETA) * (TC1 – TPEAK))) #50
+				b = a * math.exp((ALPHA + BETA1) * TC2) / (1.0 + math.exp((ALPHA + BETA) * (TC2 – TPEAK)))
+			result = CAY * math.exp(ALPHA1 * T) #60
+		elif (T + DT2 – TC2 <= 0) : #70; directs to 80, 80, 90
+			result = CAY * a * math.exp(ALPHA * T) / (1.0 + math.exp(( ALPHA + BETA) * (T – TPEAK))) #80
+		else :
+			result = CAY * b * math.exp(-BETA1 * T) #90
+	else : #100; only reaches here if IFUN == 4
+		result = CAY * math.exp(TABLIN(SOF, 2, NSOF, T, 2))
+	return result
+
