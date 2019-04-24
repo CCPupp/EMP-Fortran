5766842c409cb7bf323131a24c772feedc5056a9
CJ Erskine <cjerskine@gmail.com>
2019-04-24 12:30:00 -0400

    Remove Old Files

--- Dawson.py
+++
@@ -1,7 +0,0 @@
-# A short utility to calculate the Dawson Integral using Python libraries.
-# Last compiled and run on Ubuntu Linux. Uses Python 3. Requires Scipy to be installed
-# From Terminal, type "sudo apt-get install python-scipy" before running for the first time.
-import sys
-from scipy import special
-x = round(float(sys.argv[1]))
-print(special.dawsn(x))
