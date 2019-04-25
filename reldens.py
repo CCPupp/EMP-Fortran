2152c6b9e6c349a5b049313ece6d442dd68000f4
Lawson <woodward.41@wright.edu>
2019-04-25 16:15:22 -0400

    Added tablin function calls

--- reldens.py
+++ reldens.py
@@ -7,6 +7,7 @@ Possible problems:
 --Several references to R(1) in the original code, which I changed to r[0] in Python. Similarly, RHO(1, X) becomes rho[0][X], and DRRHO(1, X) becomes drrho[0][X].
 --Some loop variables are used as both array indeces and in equations, but arrays start at 1 in FORTRAN instead of 0 in Python. I believe I've adjusted appropriately, but any errors should look here first.
 --Several calls to TABLIN, at this time we're unsure if we're translating the function or able to use a similar one in Python
+----Update: Dylan has written a Python version of TABLIN
 
 Original FORTRAN code (block of COMMON variables omitted):
 DO  10  M = 1, NRAD
@@ -47,6 +48,7 @@ END
 '''
 
 import math
+import tablin
 
 def reldens(nrad, rad, stdrho, ntm1, dnt, r, hob, dr2, rho, drrho, dr, ndr, nt, rhoh, drrhoh) :
 	m = 0
@@ -58,15 +60,15 @@ def reldens(nrad, rad, stdrho, ntm1, dnt, r, hob, dr2, rho, drrho, dr, ndr, nt, 
 		cost = math.cos((j + 1) * dnt)
 		h1 = r[0] * cost + hob
 		h2 = (r[0] - dr2) * cost + hob 
-		rho[0][j] = math.exp(TABLIN(rad, 2, nrad, h1, 1))
-		rhod = math.exp(TABLIN(rad, 2, nrad, h2, 1))
+		rho[0][j] = math.exp(tablin.tablin(rad, 2, nrad, h1, 1))
+		rhod = math.exp(tablin.tablin(rad, 2, nrad, h2, 1))
 		drrho[0][j] = rhod * dr
 		i = 1
 		while (i < ndr) :
 			h1 = r[i] * cost + hob
 			h2 = (r[i] - dr2) * cost + hob 
-			rho[i][j] = math.exp(TABLIN(rad, 2, nrad, h1, 1))
-			rhod = math.exp(TABLIN(rad, 2, nrad, h2, 1))
+			rho[i][j] = math.exp(tablin.tablin(rad, 2, nrad, h1, 1))
+			rhod = math.exp(tablin.tablin(rad, 2, nrad, h2, 1))
 			drrho[i][j] = rhod * dr + drrho[i-1][j]
 			i += 1 #20
 		j += 1 #30
@@ -75,16 +77,15 @@ def reldens(nrad, rad, stdrho, ntm1, dnt, r, hob, dr2, rho, drrho, dr, ndr, nt, 
 		cost = math.cos((j + 0.5) * dnt)
 		h1 = r[0] * cost + hob
 		h2 = (r[0] – dr2) * cost + hob
-		rhoh[0][j] = math.exp(TABLIN(rad, 2, nrad, h1, 1))
-		rhod = math.exp(TABLIN(rad, 2, nrad, h2, 1))
+		rhoh[0][j] = math.exp(tablin.tablin(rad, 2, nrad, h1, 1))
+		rhod = math.exp(tablin.tablin(rad, 2, nrad, h2, 1))
 		drrhoh[0][j] = rhod * dr
 		i = 1
 		while (i < ndr) :
 			h1 = r[i] * cost + hob
 			h2 = (r[i] – dr2) * cost + hob
-			rhoh[i][j] = math.exp(TABLIN(rad, 2, nrad, h1, 1))
-			rhod = math.exp(TABLIN(rad, 2, nrad, h1, 1))
+			rhoh[i][j] = math.exp(tablin.tablin(rad, 2, nrad, h1, 1))
+			rhod = math.exp(tablin.tablin(rad, 2, nrad, h1, 1))
 			drrhoh[i][j] = rhod * dr + drrhoh[i-1][j]
 			i += 1 #40
 		j += 1 #50
-	
