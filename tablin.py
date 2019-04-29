0d2c8d7382b7d6929ebf50a08394f048726fd2ac
dylbo-baggins <dylan.dunham@gmail.com>
2019-04-29 16:42:30 -0400

    Update tablin.py
    corrected arrays with the loops

--- tablin.py
+++ tablin.py
@@ -1,103 +1,103 @@
 import sys
 def tablin(table, i, n, h, ia):
-    if(table(1,1)-table(1, n)<0):
-	    l = 1
-        while(l!=n):
-            if(h-table(1, l)<0):
-			    if(l-1<0 or l-1>0):
-				    s = (table(2, l)-table(2, l-1)/(table(1,l)-table(1,l-1)))
-					tablinArray = table(2, n) + (h-table(1,n))*s
+	if(table[0][0]-table[0][n]<0):
+		l = 0
+		while(l!=n):
+			if(h-table[0][1]<0):
+				if(l-1<0 or l-1>0):
+					s = (table[1][l]-table[1][l-1]/(table[1][l]-table[0][l-1]))
+					tablinArray = table[1][n] + (h-table[0][n])*s
 					return tablinArray
 				elif (l-1==0):
-				    if(ia==1):
-					    s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-						tablinArray = table(2, n) + (h-table(1,n))*s
-					    return tablinArray
+					if(ia==1):
+						s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+						tablinArray = table[1][n] + (h-table[0][n])*s
+						return tablinArray
 					elif (ia ==2):
-					    if(h-table(1,1)<0 or h-table(1,1)==0):
-						    s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-						    tablinArray = table(2, n) + (h-table(1,n))*s
-					        return tablinArray
-						elif(h-table(1,1)>0):
-							n1=n – 1
-							s=(table(2, n) – table(2, n1) ) / (table(1, n) – table(1, n1) )
-							tablinArray=table(2, n) + (h – table(1, n) ) * s
+						if(h-table[0][0]<0 or h-table[0][0]==0):
+							s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+							tablinArray = table[1][n] + (h-table[0][n])*s
 							return tablinArray
-			elif(h-table(1, l)==0):
-			    tablinArray = table(2,l)
+						elif(h-table[0][0]>0):
+							n1=n - 1
+							s=(table[1][n] - table[1][n1] ) / (table[0][n] - table[0][n1] )
+							tablinArray=table[1][n] + (h - table[0][n] ) * s
+							return tablinArray
+			elif(h-table[0][1]==0):
+				tablinArray = table[1][l]
 				return tablinArray
-			elif(h-table(1, l)>0):
+			elif(h-table[0][1]>0):
 				if(ia==1):
-					s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-					tablinArray = table(2, n) + (h-table(1,n))*s
+					s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+					tablinArray = table[1][n] + (h-table[0][n])*s
 					return tablinArray
 				elif (ia ==2):
-					if(h-table(1,1)<0 or h-table(1,1)==0):
-						s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-						tablinArray = table(2, n) + (h-table(1,n))*s
+					if(h-table[0][0]<0 or h-table[0][0]==0):
+						s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+						tablinArray = table[1][n] + (h-table[0][n])*s
 						return tablinArray
-					elif(h-table(1,1)>0):
-						n1=n – 1
-						s=(table(2, n) – table(2, n1) ) / (table(1, n) – table(1, n1) )
-						tablinArray=table(2, n) + (h – table(1, n) ) * s
+					elif(h-table[0][0]>0):
+						n1=n - 1
+						s=(table[1][n] - table[1][n1] ) / (table[0][n] - table[0][n1] )
+						tablinArray=table[1][n] + (h - table[0][n] ) * s
 						return tablinArray
 			l = l+1
 		if(ia==1):
-			s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-			tablinArray = table(2, n) + (h-table(1,n))*s
+			s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+			tablinArray = table[1][n] + (h-table[0][n])*s
 			return tablinArray
 		elif (ia ==2):
-			if(h-table(1,1)<0 or h-table(1,1)==0):
-				s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-				tablinArray = table(2, n) + (h-table(1,n))*s
+			if(h-table[0][0]<0 or h-table[0][0]==0):
+				s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+				tablinArray = table[1][n] + (h-table[0][n])*s
 				return tablinArray
-			elif(h-table(1,1)>0):
-				n1=n – 1
-				s=(table(2, n) – table(2, n1) ) / (table(1, n) – table(1, n1) )
-				tablinArray=table(2, n) + (h – table(1, n) ) * s
+			elif(h-table[0][0]>0):
+				n1=n - 1
+				s=(table[1][n] - table[1][n1] ) / (table[0][n] - table[0][n1] )
+				tablinArray=table[1][n] + (h - table[0][n] ) * s
 				return tablinArray
-	elif(table(1,1)-table(1, n)==0):
-	    print("23H Range of Table is Zero")
+	elif(table[0][0]-table[0][n]==0):
+		print("23H Range of Table is Zero")
 		return
-	elif(table(1,1)-table(1, n)>0):
-	    l = 1
-	    while(l!=n):
-            if(h-table(1, l)<0):
-			    if(l-1<0 or l-1>0):
-				    s = (table(2, l)-table(2, l-1)/(table(1,l)-table(1,l-1)))
-					tablinArray = table(2, n) + (h-table(1,n))*s
+	elif(table[0][0]-table[0][n]>0):
+		l = 0
+		while(l!=n):
+			if(h-table[0][1]<0):
+				if(l-1<0 or l-1>0):
+					s = (table[1][l]-table[1][l-1]/(table[1][l]-table[0][l-1]))
+					tablinArray = table[1][n] + (h-table[0][n])*s
 					return tablinArray
 				elif (l-1==0):
-				    if(ia==1):
-					    s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-						tablinArray = table(2, n) + (h-table(1,n))*s
-					    return tablinArray
+					if(ia==1):
+						s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+						tablinArray = table[1][n] + (h-table[0][n])*s
+						return tablinArray
 					elif (ia ==2):
-					    if(h-table(1,1)<0 or h-table(1,1)==0):
-						    s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-						    tablinArray = table(2, n) + (h-table(1,n))*s
-					        return tablinArray
-						elif(h-table(1,1)>0):
-							n1=n – 1
-							s=(table(2, n) – table(2, n1) ) / (table(1, n) – table(1, n1) )
-							tablinArray=table(2, n) + (h – table(1, n) ) * s
+						if(h-table[0][0]<0 or h-table[0][0]==0):
+							s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+							tablinArray = table[1][n] + (h-table[0][n])*s
 							return tablinArray
-			elif(h-table(1, l)==0):
-                tablinArray = table(2,l)
+						elif(h-table[0][0]>0):
+							n1=n - 1
+							s=(table[1][n] - table[1][n1] ) / (table[0][n] - table[0][n1] )
+							tablinArray=table[1][n] + (h - table[0][n] ) * s
+							return tablinArray
+			elif(h-table[0][1]==0):
+				tablinArray = table[1][l]
 				return tablinArray
-            elif(h-table(1, l)>0):
+			elif(h-table[0][1]>0):
 				if(ia==1):
-					s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-					tablinArray = table(2, n) + (h-table(1,n))*s
+					s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+					tablinArray = table[1][n] + (h-table[0][n])*s
 					return tablinArray
 				elif (ia ==2):
-					if(h-table(1,1)<0 or h-table(1,1)==0):
-						s=(table(2, n) – table(2, 1) ) / (table(1, n) – table(1, 1) )
-						tablinArray = table(2, n) + (h-table(1,n))*s
+					if(h-table[0][0]<0 or h-table[0][0]==0):
+						s=(table[1][n] - table[1][0] ) / (table[0][n] - table[0][0] )
+						tablinArray = table[1][n] + (h-table[0][n])*s
 						return tablinArray
-					elif(h-table(1,1)>0):
-						n1=n – 1
-						s=(table(2, n) – table(2, n1) ) / (table(1, n) – table(1, n1) )
-						tablinArray=table(2, n) + (h – table(1, n) ) * s
+					elif(h-table[0][0]>0):
+						n1=n - 1
+						s=(table[1][n] - table[1][n1] ) / (table[0][n] - table[0][n1] )
+						tablinArray=table[1][n] + (h - table[0][n] ) * s
 						return tablinArray
 			l = l+1
